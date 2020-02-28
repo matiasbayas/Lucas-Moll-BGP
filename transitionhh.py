0790dfbb2f92264c83785181d99ee22d8e6b3613
matiasbayas <matias.bayas.erazo@gmail.com>
2020-02-28 15:31:36 -0600

    First attempt at solving for value functions, given distribution

---
+++ transitionhh.py
@@ -0,0 +1,21 @@
+
+
+# The idea is that you will eventually go to new ss.
+# So first solve for the steady state under the new tail parameter:
+
+import growthmethods as gm
+
+# Final Tail Parameter:
+theta = 0.7
+
+# Other Parameters:
+rho = 0.06
+k = 0.05
+eta = 0.3
+alpha0 = 0.0849
+
+xi = 0.4
+
+I = 2001
+xmin = 0
+xmax = 3
